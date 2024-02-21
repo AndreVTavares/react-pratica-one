306cac4b5009c16820f3977399fce975748a710d
André Tavares <andretavares3@gmail.com>
2024-02-21 20:43:07 -0300

    getting server.json data and showing on screen

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './app'
 import './index.css'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+
   </React.StrictMode>,
 )
