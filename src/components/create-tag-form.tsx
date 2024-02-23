fbbc82ba647f333048313e57c349f49af797cf6a
André Tavares <andretavares3@gmail.com>
2024-02-22 21:24:22 -0300

    implementing create Tag function and showing error message

--- src/components/create-tag-form.tsx
+++ src/components/create-tag-form.tsx
@@ -1,4 +1,4 @@
-import { Check, X } from 'lucide-react'
+import { Check, Loader2, X } from 'lucide-react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
@@ -6,50 +6,62 @@ import { Button } from "./ui/button";
 import * as Dialog from '@radix-ui/react-dialog'
 
 const createTagSchema = z.object({
-    name: z.string().min(3, { message: 'Minimum 3 characters' }),
-    slug: z.string(),
+    title: z.string().min(3, { message: 'Minimum 3 characters' }),
 })
 
 type CreateTagSchema = z.infer<typeof createTagSchema>
 
 function getSlugFromString(input: string): string {
-    return  input
-      .normalize("NFD")
-      .replace(/[\u0300-\u036f]/g, "")
-      .toLowerCase()
-      .replace(/[^\w\s]/g, '')
-      .replace(/\s+/g, '-');
-  }
+    return input
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "")
+        .toLowerCase()
+        .replace(/[^\w\s]/g, '')
+        .replace(/\s+/g, '-');
+}
+
+
+
 
 export function CreateTagForm() {
-    const { register, handleSubmit, watch } = useForm<CreateTagSchema>({
+    const { register, handleSubmit, watch, formState } = useForm<CreateTagSchema>({
         resolver: zodResolver(createTagSchema)
     })
 
-    function createTag(data: CreateTagSchema) {
-        console.log(data)
+    const slug = watch('title')
+        ? getSlugFromString(watch('title'))
+        : ''
+
+    async function createTag({ title}: CreateTagSchema) {
+        await fetch('http://localhost:3333/tags', {
+            method: 'POST',
+            body: JSON.stringify({
+                title,
+                slug,
+                amountOfVideos: 0,
+            })
+        })
     }
 
-    const slug = watch('name') 
-    ? getSlugFromString(watch('name')) 
-    : ''
-
+    
     return (
         <form onSubmit={handleSubmit(createTag)} className='w-full space-y-6'>
             <div className='space-y-2'>
-                <label htmlFor='name' className='text-sm font-medium block'>Tag name</label>
+                <label htmlFor='title' className='text-sm font-medium block'>Tag name</label>
                 <input
-                    {...register('name')}
+                    {...register('title')}
                     id='name'
                     type='text'
                     className='border border-zinc-800 rounded-lg px-3 py-2.5 text-sm bg-zinc-800/50 w-full'
                 />
+                {formState.errors?.title && (
+                    <p className='text-sm text-red-400'>{formState.errors.title.message}</p>
+                )}
             </div>
 
             <div className='space-y-2'>
                 <label htmlFor='slug' className='text-sm font-medium' >Slug</label>
                 <input
-                    {...register('slug')}
                     id='slug'
                     type='text'
                     value={slug}
@@ -66,8 +78,8 @@ export function CreateTagForm() {
                     </Button>
                 </Dialog.Close>
 
-                <Button className='bg-teal-400 text-teal-950'>
-                    <Check className='size-3' />
+                <Button disabled={formState.isSubmitting} className='bg-teal-400 text-teal-950'>
+                    {formState.isSubmitting ? <Loader2 className='size-3 animate-spin'/> : <Check className='size-3' />}
                     Save
                 </Button>
             </div>
